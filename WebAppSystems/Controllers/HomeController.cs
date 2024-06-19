f7648fbfc8c9115d34b1f363ed547ceabf93e363
Jackson Ricardo Wippel <jrwippel@hotmail.com>
2024-06-18 22:09:11 -0300

    Ajuste formato horas clientes exbidos na paginal inicial - grafico

--- WebAppSystems/Controllers/HomeController.cs
+++ WebAppSystems/Controllers/HomeController.cs
@@ -11,13 +11,14 @@ namespace WebAppSystems.Controllers
         public HomeController(ProcessRecordsService processRecordsService)
         {
             _processRecordsService = processRecordsService;
-        }        
+        }
         public async Task<IActionResult> Index()
         {
             var chartData = _processRecordsService.GetChartData();
-
             return View(chartData);
         }
+
+
         public IActionResult About()
         {           
             return View();
